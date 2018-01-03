7c82aea092ca1192d16a73ca7a11ba3653577d09
GREATYUN\윤지상 <yjs8237@gmail.com>
2018-01-03 17:33:42 +0900

    asd

--- src/com/isi/main/XMLSvcMain.java
+++ src/com/isi/main/XMLSvcMain.java
@@ -97,7 +97,7 @@ public class XMLSvcMain {
 				int result = alive.startAliveProc(); 
 				if(result == RESULT.RTN_SUCCESS){
 					duplexMgr.setStandByMode();
-				} else { //test
+				} else { //
 					if(result == RESULT.TCP_CONN_FAIL){
 						System.out.println("TCP Connection FAIL!!");
 						duplexMgr.setActiveMode();
